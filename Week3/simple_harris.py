04ebda98745e0d11911f1e57e51b1c351bf14a97
Thinh Nguyen <thinhntr@gmail.com>
2020-11-18 20:10:19 +0700

    Backup

--- Week3/simple_harris.py
+++ Week3/simple_harris.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2
 
+def show(*images):
+    rows = len(images)
+    fig = plt.figure()
+    for i in range(rows):
+        ax = fig.add_subplot(rows, 1, i + 1)
+        if len(images[i].shape) == 3:
+            ax.imshow(images[i])
+        else:
+            ax.imshow(images[i], cmap='gray')
+
 def gaussian_kernel(sigma=3, size=3):
     kernel = np.zeros((size, size))
     double_sigma_2 = 2 * sigma ** 2
@@ -100,17 +110,43 @@ def get_M(r, c, src, Ix, Iy, weights):
 
 
 
-def simple_harris(src):
+def simple_harris(src,  k=0.04):
     img = src.astype(np.float)
     response = np.zeros_like(img)
     # Ix, Iy = derivative(img)
     Ix = cv2.Scharr(src, cv2.CV_64F, 1, 0)
     Iy = cv2.Scharr(src, cv2.CV_64F, 0, 1)
-    w = gaussian_kernel()
+    
+    Ix2 = Ix * Ix
+    Iy2 = Iy * Iy
+    Ixy = Ix * Iy
 
-    for r in range(src.shape[0]):
-        for c in range(src.shape[1]):
-            M = get_M(r, c, img, Ix, Iy, w)
-            response[r, c] = np.linalg.det(M) - (np.trace(M))
+    for r in range(1, src.shape[0] - 1):
+        for c in range(1, src.shape[1] - 1):
+            Sx2 = get_window(Ix2, r, c).sum()
+            Sy2 = get_window(Iy2, r, c).sum()
+            Sxy = get_window(Ixy, r, c).sum()
+            M = np.array([[Sx2, Sxy], [Sxy, Sy2 ]])
+            response[r, c] = np.linalg.det(M) - k * np.trace(M) ** 2
 
-    return response       
+    return response
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    img = cv2.imread('images/checkerboard.png')
+
+    assert(img is not None)
+
+    img = cv2.resize(img, (500, 500))
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    response = simple_harris(gray)
+
+    copy = np.array(img)
+    a = np.count_nonzero(response > 0.01 * response.max())
+    
+    print(a)
+    # show(copy)
+    # plt.show()
+
